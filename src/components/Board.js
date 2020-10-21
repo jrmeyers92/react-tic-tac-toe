6af5a52662e6334f86f525c504858833a737dc72
Jake Meyers <jakejake92@yahoo.com>
2020-10-21 16:09:40 -0600

    refactor with map

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,9 +2,24 @@ import React from "react";
 import Square from "./Square";
 
 const Board = () => {
-	const style = {};
+	const style = {
+		display: "flex",
+		flexWrap: "wrap",
+		border: "2px solid darkBlue",
+	};
+	const squares = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-	return <div style={style}></div>;
+	return (
+		<div
+			style={style}
+			onClick={(e) => {
+				console.log(e.target.value);
+			}}>
+			{squares.map((square) => (
+				<Square value={square}>square</Square>
+			))}
+		</div>
+	);
 };
 
 export default Board;
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,16 +1,20 @@
 import React from "react";
 
-const Square = (props) => {
+const Square = () => {
 	const style = {
 		background: "lightBlue",
 		fontSize: "30px",
 		border: "3px solid Darkblue",
 		cursor: "pointer",
+		height: "70px",
+		width: "30vw",
+		height: "30vh",
+		border: "2px solid darkBlue",
 	};
 
 	return (
 		<div>
-			<button style={style}>{props.number}</button>
+			<button style={style}></button>
 		</div>
 	);
 };
